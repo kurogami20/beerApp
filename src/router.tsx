e17a7ea223606f00338c1825145d90ec0200ed7a
kurogami20 <dam31vt@gmail.com>
2026-04-14 16:08:39 +0200

    Add dependencies and setup routing for the application

--- src/router.tsx
+++ src/router.tsx
@@ -1,8 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-
+import { BrowserRouter, Route, Routes } from 'react-router';
 const rootElement = document.getElementById('root');
 if (rootElement) {
-	createRoot(rootElement).render(<StrictMode></StrictMode>);
+	createRoot(rootElement).render(
+		<StrictMode>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<div>Home</div>} />
+				</Routes>
+			</BrowserRouter>
+		</StrictMode>,
+	);
 }
