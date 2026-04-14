5eb8bbb2bd54c5236b7bbb5f79496ffbe6880e92
kurogami20 <dam31vt@gmail.com>
2026-04-14 15:48:02 +0200

    project init

---
+++ dockerfile
@@ -0,0 +1,16 @@
+FROM node:25-alpine
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+EXPOSE 5173
+
+CMD ["npm", "run", "dev"]
+
+
+
---
+++ src/router.tsx
@@ -0,0 +1,8 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+	createRoot(rootElement).render(<StrictMode></StrictMode>);
+}
